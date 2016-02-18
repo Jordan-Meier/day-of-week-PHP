1caf529f6e7fbb5032dc0a6662dfd6420b41495c
Jordan Meier and Jon Nardine <jwnardine@gmail.com>
2016-02-18 10:42:17 -0800

    fix spacing

--- src/DateFind.php
+++ src/DateFind.php
@@ -6,6 +6,4 @@
             return date('F', mktime(0, 0, 0, $month, $day, $year)) . " $day, $year is on a " . date('l', mktime(0, 0, 0, $month, $day, $year));
         }
     }
-
-
  ?>
--- tests/DateFindTest.php
+++ tests/DateFindTest.php
@@ -4,8 +4,6 @@
 
     class DateFindTest extends PHPUnit_Framework_TestCase
     {
-
-
         function test_dayOfWeekFinder_normalYear()
         {
             //Arrange
@@ -16,7 +14,6 @@
 
             //Act
             $result = $test_DateFind->dayOfWeekFinder($month, $day, $year);
-
 
             //Assert
             $this->assertEquals("July 5, 1967 is on a Wednesday", $result);
@@ -34,13 +31,9 @@
             //Act
             $result = $test_DateFind->dayOfWeekFinder($month, $day, $year);
 
-
             //Assert
             $this->assertEquals("March 20, 2020 is on a Friday", $result);
 
         }
-
-
     }
-
 ?>
